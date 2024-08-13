0ec67f735df6211dcc7190e6c688238f66e49da8
Nguyen Tien Phong <321phong@gmail.com>
2024-08-13 07:48:02 +0700

    Phongyv

---
+++ src/Backend/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7Rfx9IgKLSUpEyTgaoRt5hOJ0v6vfK7s",
+  authDomain: "phentai-54ded.firebaseapp.com",
+  projectId: "phentai-54ded",
+  storageBucket: "phentai-54ded.appspot.com",
+  messagingSenderId: "12964111041",
+  appId: "1:12964111041:web:90d6736f6990d568246a40",
+  measurementId: "G-VLF6ZY1BM7"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
